8f5d5647e0d5101a6fad36bf08bf5deccfa22b27
Avinash Kumar <61631621+isinghking007@users.noreply.github.com>
2021-08-17 23:18:40 +0530

    Add files via upload

---
+++ HexValue.java
@@ -0,0 +1,17 @@
+package jspiders;
+import java.io.*;
+import java.util.*;
+public class HexValue {
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the starting character");
+		char start=in.next().toUpperCase().charAt(0);
+		System.out.println("Enter the last character");
+		char end=in.next().toUpperCase().charAt(0);
+		for(char c=start;c<=end;c++)
+		{
+			System.out.println(Integer.toHexString((int)c));
+		}
+	}
+}
